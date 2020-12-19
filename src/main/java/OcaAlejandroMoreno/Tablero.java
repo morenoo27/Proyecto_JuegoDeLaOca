0e42c7b33c1e9011786ad419cde698ab22dd3020
Moreno27 <alejanmm2000@gmail.com>
2020-12-19 11:05:43 +0100

    Creacion del proyercto, creamos las clases tablero, casilla y jugador

---
+++ src/main/java/OcaAlejandroMoreno/Tablero.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OcaAlejandroMoreno;
+
+/**
+ *
+ * @author aleja
+ */
+public class Tablero {
+    
+    private Casilla casilla;
+    private Jugador jugador;
+    
+    public Tablero(){
+        
+    }
+}
