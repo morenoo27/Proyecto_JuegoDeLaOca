2719cffc82d2d3c0baa4016929084736273bbbf4
Moreno27 <alejanmm2000@gmail.com>
2020-12-25 19:58:03 +0100

    cambio de variable a final mas el to String

--- src/main/java/oca/Casilla.java
+++ src/main/java/oca/Casilla.java
@@ -8,17 +8,17 @@ package oca;
  */
 final class Casilla {
 
-    private boolean sentencia; //para si la casilla teniene una accion o no
-    private boolean pierdeTurno;//true pierde turno
-    private boolean vuelveATirar;//true vuelve a tirar
+    private final boolean SENTENCIA; //para si la casilla teniene una accion o no
+    private final boolean PIERDETURNO;//true pierde turno
+    private final boolean VUELVEATIRAR;//true vuelve a tirar
 
-    //la casilla por defecto sera la normal, no tienen sentencia y una vez 
+    //la casilla por defecto sera la normal, no tienen SENTENCIA y una vez 
     //movida la ficha, de pierde el turno y tira otrto jugador
     public Casilla() {
         
-        this.sentencia = false;
-        this.pierdeTurno = true;
-        this.vuelveATirar = false;
+        this.SENTENCIA = false;
+        this.PIERDETURNO = true;
+        this.VUELVEATIRAR = false;
     }
 
     //luego, habra casillas que seran diferentes a las demas, por lo tanto,
@@ -26,8 +26,13 @@ final class Casilla {
 
     public Casilla(boolean sentencia, boolean pierdeTurno, boolean vuelveATirar) {
         
-        this.sentencia = sentencia;
-        this.pierdeTurno = pierdeTurno;
-        this.vuelveATirar = vuelveATirar;
+        this.SENTENCIA = sentencia;
+        this.PIERDETURNO = pierdeTurno;
+        this.VUELVEATIRAR = vuelveATirar;
+    }
+
+    @Override
+    public String toString() {
+        return "Casilla {" + "sentencia = " + SENTENCIA + ", pierdeTurno = " + PIERDETURNO + ", vuelveATirar = " + VUELVEATIRAR + '}';
     }
 }
