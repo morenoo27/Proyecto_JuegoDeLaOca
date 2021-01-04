b0bcb58127f5d1aa0c1b1b2e80c4d3cbe1cd0230
Moreno27 <alejanmm2000@gmail.com>
2021-01-04 16:12:01 +0100

    creada la sentencia para finalizar el juego

--- src/main/java/oca/Casilla.java
+++ src/main/java/oca/Casilla.java
@@ -11,6 +11,7 @@ final class Casilla {
     private final boolean SENTENCIA; //para si la casilla teniene una accion o no
     private final boolean PIERDETURNO;//true pierde turno
     private final boolean VUELVEATIRAR;//true vuelve a tirar
+    private final boolean FINAL;
 
     //la casilla por defecto sera la normal, no tienen SENTENCIA y una vez 
     //movida la ficha, de pierde el turno y tira otrto jugador
@@ -19,16 +20,16 @@ final class Casilla {
         this.SENTENCIA = false;
         this.PIERDETURNO = true;
         this.VUELVEATIRAR = false;
+        this.FINAL = false;
     }
 
     //luego, habra casillas que seran diferentes a las demas, por lo tanto,
     //recibiran valores parametrizados
-
-    public Casilla(boolean sentencia, boolean pierdeTurno, boolean vuelveATirar) {
-        
-        this.SENTENCIA = sentencia;
-        this.PIERDETURNO = pierdeTurno;
-        this.VUELVEATIRAR = vuelveATirar;
+    public Casilla(boolean SENTENCIA, boolean PIERDETURNO, boolean VUELVEATIRAR, boolean FINAL) {
+        this.SENTENCIA = SENTENCIA;
+        this.PIERDETURNO = PIERDETURNO;
+        this.VUELVEATIRAR = VUELVEATIRAR;
+        this.FINAL = FINAL;
     }
 
     @Override
