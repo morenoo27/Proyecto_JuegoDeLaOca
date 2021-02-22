d3065d472da62d1d2268d00f2f4ce76dff7e6e98
morenoo27 <alejanmm2000@gmail.com>
2021-02-22 20:06:10 +0100

    reescritura de casilla, casilla basada en proyecto de Vico

--- src/main/java/oca/Casilla.java
+++ src/main/java/oca/Casilla.java
@@ -3,76 +3,76 @@
  */
 package oca;
 
+import java.util.ArrayList;
+
 /*
 
  */
 final class Casilla {
 
-    private final boolean SENTENCIA; //para si la casilla teniene una accion o no
-    private final int PIERDETURNO;//true pierde turno
-    private final boolean VUELVEATIRAR;//true vuelve a tirar
-    private final boolean SALTODECASILLA;//true hay salto, false, no
-    private final boolean DEPENDEDEJUGADOR;
-    private final boolean FINAL;
+    private int id;
+    private TiposDeCasillas tipoDeCasilla;
+    private ArrayList<Jugador> jugadores;
+
+    public Casilla(int id, TiposDeCasillas casilla) {
+        this.id = id;
+        this.tipoDeCasilla = casilla;
+        this.jugadores = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public TiposDeCasillas getTipoDeCasilla() {
+        return tipoDeCasilla;
+    }
+
+    public void setTipoDeCasilla(TiposDeCasillas casilla) {
+        this.tipoDeCasilla = casilla;
+    }
+
+    public ArrayList<Jugador> getJugadores() {
+        return jugadores;
+    }
+
+    public void setJugadores(ArrayList<Jugador> jugadores) {
+        this.jugadores = jugadores;
+    }
     
-
-    //la casilla por defecto sera la normal, no tienen SENTENCIA y una vez 
-    //movida la ficha, de pierde el turno y tira otrto jugador
-    public Casilla() {
-
-        this.SENTENCIA = false;
-        this.PIERDETURNO = 1;
-        this.VUELVEATIRAR = false;
-        this.SALTODECASILLA = false;
-        this.DEPENDEDEJUGADOR = false;
-        this.FINAL = false;
-    }
-
-    //luego, habra casillas que seran diferentes a las demas, por lo tanto,
-    //recibiran valores parametrizados
-    public Casilla(boolean SENTENCIA, int PIERDETURNO, boolean VUELVEATIRAR, boolean SALTODECASILLA, boolean DEPENDEDEJUGADOR, boolean FINAL) {
+    public void addJugador(Jugador jugador){
         
-        this.SENTENCIA = SENTENCIA;
-        this.PIERDETURNO = PIERDETURNO;
-        this.VUELVEATIRAR = VUELVEATIRAR;
-        this.SALTODECASILLA = SALTODECASILLA;
-        this.DEPENDEDEJUGADOR = DEPENDEDEJUGADOR;
-        this.FINAL = FINAL;
+        this.jugadores.add(jugador);
     }
-
-    public boolean getSENTENCIA() {
+    
+    public void removeJugador(Jugador jugador){
         
-        return this.SENTENCIA;
-    }
-
-    public int getPIERDETURNO() {
-        
-        return this.PIERDETURNO;
-    }
-
-    public boolean getVUELVEATIRAR() {
-        
-        return this.VUELVEATIRAR;
-    }
-
-    public boolean getSALTODECASILLA() {
-        
-        return this.SALTODECASILLA;
-    }
-
-    public boolean getDEPENDEDEJUGADOR() {
-        
-        return DEPENDEDEJUGADOR;
-    }
-
-    public boolean getFINAL() {
-        
-        return this.FINAL;
+        this.jugadores.remove(jugador);
     }
 
     @Override
     public String toString() {
-        
-        return "Casilla {" + "sentencia = " + SENTENCIA + ", pierdeTurno = " + PIERDETURNO + ", vuelveATirar = " + VUELVEATIRAR + '}';
+        return id + "\t" + tipoDeCasilla + "\t" + mostraJugadores();
     }
+
+    private String mostraJugadores() {
+
+        String tmp = "|";
+        
+        if(!this.jugadores.isEmpty()){
+            
+            for (int i = 0; i < jugadores.size(); i++) {
+                
+                tmp += " " + this.jugadores.get(i).getApodo();
+            }
+        }
+        
+        tmp += " |";
+        return tmp;
+    }
+
 }
