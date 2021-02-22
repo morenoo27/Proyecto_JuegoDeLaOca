ead7348b4f25204fce8e6e5eb452e1dd5ae28800
morenoo27 <alejanmm2000@gmail.com>
2021-02-22 20:08:27 +0100

    metodo de jugar ronda, aun en proceso

--- src/main/java/oca/Jugador.java
+++ src/main/java/oca/Jugador.java
@@ -4,6 +4,7 @@
 package oca;
 
 import java.util.Random;
+import static oca.Juego.tablero;
 
 /**
  *
@@ -15,6 +16,7 @@ class Jugador {
     private char apodo;
     private int posicion;//no se si realmente es int, posible cambio
     private int tiradaInicial;
+    private int turnosSinJugar;
 
     //vamosa a tener que crear un objeto de tipo random
     static Random random = new Random();
@@ -26,6 +28,7 @@ class Jugador {
         this.apodo = apodo;
         this.posicion = 0;
         this.tiradaInicial = 0;
+        this.turnosSinJugar = 0;
     }
 
     public int tirarDados() {
@@ -34,11 +37,76 @@ class Jugador {
         return random.nextInt(7);//devuelve un numero entre 0-6
     }
 
+    public void jugarTruno() {
+
+        boolean tiraDeNuevo = false;
+
+        do {
+            //pillamos la posicion actual(y su casilla correspondiente)
+            int posi = this.posicion;
+
+            Casilla actual = tablero.getCasilla(posi);
+            actual.removeJugador(this);
+
+            int tirada = this.tirarDados();
+            if (sePasa(tirada, actual)) {
+
+                //calculamos cuanto hay hasta el jardin
+                int hastaJardin = tablero.getCasilla(tablero.getCasillas() - 1).getId() - actual.getId();
+
+                //nos movemos hasta el jardin
+                this.moverse(hastaJardin);
+
+                //calculamos el rebote
+                int rebote = tirada - hastaJardin;
+
+                //retrocedemos la cantidad
+                this.retroceder(rebote);
+
+            } else {
+
+                this.moverse(tirada);
+            }
+
+            //ahora recibimos la casilla en la que caemos y comprobamos si
+            //tiene sentencia o es una casilla normal y que pase al siguiente jugador
+            Casilla caida = tablero.getCasilla(this.posicion);
+
+            if (caida.getTipoDeCasilla().isSentencia()) {
+
+                //miramos si tenemos que movernos
+                if (caida.getTipoDeCasilla().getSiguienteMovimiento() != 0) {
+
+                    if (caida.getTipoDeCasilla().getSiguienteMovimiento() == -12) {
+                        System.out.println("Has caido en el laberinto..."
+                                + "\nEstas mas perdio que el barco del arro hulio");
+                        this.retroceder(12);
+                    } else {
+                        this.moverse(caida.getTipoDeCasilla().getSiguienteMovimiento());
+                    }
+                }
+
+                //ahora miramos si perdemos turnos
+                if (caida.getTipoDeCasilla().getTurnosSinJugar() != 0) {
+                    this.setTurnosSinJugar(caida.getTipoDeCasilla().getTurnosSinJugar());
+                }
+                
+                //ahora miramos si tiramos de nuevo
+                tiraDeNuevo = caida.getTipoDeCasilla().isTiradaExtra();
+                
+            } else {
+                System.out.println("Has caido en una casilla normal");
+            }
+            
+            caida.addJugador(this);
+        } while (tiraDeNuevo);
+    }
+
     public void moverse(int movimiento) {
 
         this.posicion += movimiento;
     }
-    
+
     public void retroceder(int movimiento) {
 
         this.posicion -= movimiento;
@@ -56,8 +124,37 @@ class Jugador {
         this.tiradaInicial = tiradaInicial;
     }
 
+    public int getTurnosSinJugar() {
+        return turnosSinJugar;
+    }
+
+    public void setTurnosSinJugar(int turnosSinJugar) {
+        this.turnosSinJugar = turnosSinJugar;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public char getApodo() {
+        return apodo;
+    }
+
+    public void setApodo(char apodo) {
+        this.apodo = apodo;
+    }
+
     @Override
     public String toString() {
         return apodo + ", posicion: " + posicion;
+    }
+
+    private boolean sePasa(int tirada, Casilla actual) {
+
+        return actual.getId() + tirada > tablero.getCasillas() - 1;
     }
 }
