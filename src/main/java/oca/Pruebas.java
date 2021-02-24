d8ebe272187d5a1a46c25fcc207595a24967bc0f
morenoo27 <alejanmm2000@gmail.com>
2021-02-24 17:29:29 +0100

    Pruebas

--- src/main/java/oca/Pruebas.java
+++ src/main/java/oca/Pruebas.java
@@ -5,6 +5,7 @@
  */
 package oca;
 
+import javax.swing.JOptionPane;
 import static oca.Juego.tablero;
 
 /**
@@ -12,8 +13,30 @@ import static oca.Juego.tablero;
  * @author aleja
  */
 public class Pruebas {
+
+    static Tablero tablero = new Tablero();//
+
     public static void main(String[] args) {
-        System.out.println(tablero.getCasilla(tablero.getCasillas() - 1).getId());
+
+//        //vamos a tener aqui un jugador predeterminado para realizar pruebas de
+//        //que el funcionamiento de las casillas es valido 
+//        Jugador prueba = new Jugador("alejandro", 'm');
+//
+//        boolean fin = true;
+//        int t = 1;
+//        do {
+//
+//            prueba.jugarTruno();
+//
+//            
+//
+//            if (prueba.getPosicion() == tablero.getCasillas() - 1) {
+//                fin = prueba.ganarPartida();
+//            }
+//
+//        } while (fin);
         
+        
+        JOptionPane.showMessageDialog(null, tablero, "Turno", 0);
     }
 }
