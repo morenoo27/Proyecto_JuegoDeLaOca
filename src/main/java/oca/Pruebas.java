6b8d89217ff80dcf1ed8913b1baec6d0e6ca2cb3
Moreno27 <alejanmm2000@gmail.com>
2021-02-25 16:03:20 +0100

    prueba de greaficos

--- src/main/java/oca/Pruebas.java
+++ src/main/java/oca/Pruebas.java
@@ -5,38 +5,35 @@
  */
 package oca;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import static oca.Juego.tablero;
-
 /**
  *
  * @author aleja
  */
 public class Pruebas {
 
-    static Tablero tablero = new Tablero();//
+    
+    private static final ImageIcon ICONO = new ImageIcon("src/main/fotos/patata2.jpg");
+
+    private static final JLabel a = new JLabel("<html>"
+            + "    <pre>"
+            + "             |25| |24| |23| |22| |21| |20| |19| |18|\n"
+            + "        |26|      |49| |48| |47| |46| |45| |44|      |17|\n"
+            + "        |27| |50|         ________              |43| |16|\n"
+            + "        |28| |51|        |        |             |42| |15|\n"
+            + "        |29| |52|        |   63  |62| |61| |60| |41| |14|\n"
+            + "        |30| |53|        |________|      |59|   |40| |13|\n"
+            + "        |31|      |54| |55| |56| |57| |58|      |39| |12|\n"
+            + "             |32| |33| |34| |35| |36| |37| |38|      |11|\n"
+            + "   | 1| | 2| | 3| | 4| | 5| | 6| | 7| | 8| | 9| |10|"
+            + "    </pre>"
+            + "</html>");
 
     public static void main(String[] args) {
 
-//        //vamos a tener aqui un jugador predeterminado para realizar pruebas de
-//        //que el funcionamiento de las casillas es valido 
-//        Jugador prueba = new Jugador("alejandro", 'm');
-//
-//        boolean fin = true;
-//        int t = 1;
-//        do {
-//
-//            prueba.jugarTruno();
-//
-//            
-//
-//            if (prueba.getPosicion() == tablero.getCasillas() - 1) {
-//                fin = prueba.ganarPartida();
-//            }
-//
-//        } while (fin);
-        
-        
-        JOptionPane.showMessageDialog(null, tablero, "Turno", 0);
+        JOptionPane.showMessageDialog(null, a, "turno", 0, ICONO);
     }
 }
+  
